ab5c8ed7e96f12022e79b6f1616ef2d341317a60
SkimoCHK <alexguillen265@gmail.com>
2023-10-14 20:29:37 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_AGGP/MVVM_implementacion_AGGP/VistaModelo/VMpatron.cs
@@ -0,0 +1,40 @@
+﻿using MvvmGuia.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_AGGP.VistaModelo
+{
+    public class VMpatron : BaseViewModel
+    {
+
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+
+        #endregion
+        #region OBJETOS
+
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value);}
+
+        }
+        #endregion
+        #region PROCESOS
+        
+        public async Task ProcesosAsyncrono()
+        {
+
+        }
+
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
