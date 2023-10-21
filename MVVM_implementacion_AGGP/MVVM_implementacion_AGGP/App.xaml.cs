b0e66970510127736deeb72df6756b1dac603812
SkimoCHK <alexguillen265@gmail.com>
2023-10-20 19:22:19 -0700

    A

--- MVVM_implementacion_AGGP/MVVM_implementacion_AGGP/App.xaml.cs
+++ MVVM_implementacion_AGGP/MVVM_implementacion_AGGP/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVM_implementacion_AGGP.Vistas;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MVVM_implementacion_AGGP
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new pagina1();
         }
 
         protected override void OnStart()
